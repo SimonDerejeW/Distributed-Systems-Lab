1ec522eaa034b83eaa097e9e802037e6976898d0
SimonDerejeW <simondereje1@gmail.com>
2024-11-17 16:55:44 +0300

    feat: added shutdown after inactivity for the client code

--- distributed-key-value-store/client/client.go
+++ distributed-key-value-store/client/client.go
@@ -6,42 +6,69 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
 // startClient connects to the server and handles user input
 func startClient() {
 	conn, err := net.Dial("tcp", "localhost:8080")
+	
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
 		return
 	}
 	defer conn.Close()
 	fmt.Println("Connected to server. You can start sending commands (PUT, GET, DELETE, LIST)")
+	
+	timer := time.NewTimer(30 * time.Second)
+	// Channel to signal user input activity
+	reset := make(chan bool)
 
-	scanner := bufio.NewScanner(os.Stdin)
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for {
+			fmt.Print("Enter command: ")
+			scanner.Scan()
+			text := scanner.Text()
+			
+			if strings.ToUpper(text) == "EXIT" {
+				fmt.Println("Exiting client...")
+				return
+			}
+
+			reset <- true
+	
+			_, err := conn.Write([]byte(text + "\n"))
+			if err != nil {
+				fmt.Println("Error sending message:", err)
+				return
+			}
+	
+			// Read response from server
+			response, err := bufio.NewReader(conn).ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading response:", err)
+				return
+			}
+			fmt.Println("Server response:", strings.TrimSpace(response))
+		}
+
+	}()
+
 	for {
-		fmt.Print("Enter command: ")
-		scanner.Scan()
-		text := scanner.Text()
-
-		if strings.ToUpper(text) == "EXIT" {
-			fmt.Println("Exiting client...")
+		select {
+		case <-timer.C:
+			// Timer expired, exit the program
+			fmt.Println("No activity for 30 seconds. Exiting...")
 			return
+		case <-reset:
+			// User input received, reset the timer
+			if !timer.Stop() {
+				<-timer.C // Drain the channel to prevent blocking
+			}
+			// Restart the timer
+			timer.Reset(30 * time.Second)
 		}
-
-		_, err := conn.Write([]byte(text + "\n"))
-		if err != nil {
-			fmt.Println("Error sending message:", err)
-			return
-		}
-
-		// Read response from server
-		response, err := bufio.NewReader(conn).ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading response:", err)
-			return
-		}
-		fmt.Println("Server response:", strings.TrimSpace(response))
 	}
 }
 
